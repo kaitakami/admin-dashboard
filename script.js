91a60cf6fdba038a2a7ce623bae2e12a1e49ca70
Kai Takami <kaitakami@protonmail.com>
2022-06-03 09:20:10 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#nav');
+const cancelButton = document.querySelector('.cancel-btn');
+const menuButton = document.querySelector('.menu-btn');
+
+menuButton.addEventListener('click', () => {
+    menu.style.left = '0%';
+})
+
+cancelButton.addEventListener('click', () => {
+    menu.style.left = '-120%';
+})
